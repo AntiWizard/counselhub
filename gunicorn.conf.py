83be683d36acb874f34d74f57216bf3caeb8b50d
ali <aliminuei@gmail.com>
2026-01-05 14:01:29 +0330

    Fix gunicorn env

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,10 +1,11 @@
+import os
+
 import gunicorn
-from decouple import config
 
 gunicorn.SERVER_SOFTWARE = ""
 gunicorn.SERVER = ""
 bind = "0.0.0.0:8000"
 workers = 5
 preload_app = True
-proc_name = config("PROJECT_NAME")
+proc_name = os.getenv("PROJECT_NAME")
 loglevel = "info"
